1cb9621c79f65fd685d60a4b8feecc06cc3d23b5
TStappen <timvanderstappen@hotmail.nl>
2022-10-19 14:53:55 +0200

    [ADDED] Rock Paper Scissors

--- repos/wedstrijden/RockPaperScissors/Form1.cs
+++ repos/wedstrijden/RockPaperScissors/Form1.cs
@@ -9,40 +9,48 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace RockPaperScissors
-{
+{ 
     public partial class Form1 : Form
     {
+        Game m_game = new Game();
+        
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void groupBox1_Enter(object sender, EventArgs e)
-        {
-
-        }
-
-        private void groupBox2_Enter(object sender, EventArgs e)
-        {
-
+            
         }
 
         private void radioButtonPaper_Click(object sender, EventArgs e)
         {
-
+            m_game.makeChoice(CHOICE.PAPER);
         }
 
         private void radioButtonScissors_Click(object sender, EventArgs e)
         {
-
+            m_game.makeChoice(CHOICE.SCISSORS);
         }
 
         private void radioButtonRock_Click(object sender, EventArgs e)
         {
-
+            m_game.makeChoice(CHOICE.ROCK);
         }
 
         private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            // run game
+            m_game.play();
+
+            // update GUI
+            string choiceEnemy = m_game.getEnemyChoice().ToString();
+            textBoxEnemyChoice.Text = choiceEnemy;
+
+            string choicePlayer = m_game.getPlayerChoice().ToString();
+
+            string result = m_game.calcWin() ? "WIN" : "LOSE";
+            textBoxHistory.AppendText(choicePlayer + " - " + choiceEnemy + " - " + result + "\r\n");
+        }
+
+        private void radioButtonPaper_CheckedChanged(object sender, EventArgs e)
         {
 
         }
